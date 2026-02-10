962e3a1277471f7140d87c67a0bd18648bf37a1a
TOLTz <joaovictorcarrenho@gmail.com>
2026-02-10 10:38:42 -0300

    fix something

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve tudo que está em /public
 app.use(express.static(path.join(__dirname, "public")));
 
-// (Opcional) garantir que "/" abra o index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
